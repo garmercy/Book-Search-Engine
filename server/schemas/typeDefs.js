b2e8d4927382aed2767b3cec91039924e58b9c94
Cristina Garcia <garmercy.mar@gmail.com>
2024-01-23 11:47:05 +1100

    adding typeDefs server

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,44 @@
-//to do
+const typeDefs = `
+  type Query {
+    me: User
+  }
+
+  type Mutation{
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: SaveBookInput): User
+    removeBook(bookId: ID!): User
+  }
+
+  imput SaveBookInput{
+    bookId: ID!, 
+    authors: [String]!, 
+    description: String!, 
+    title: String!, 
+    image: String!, 
+    link: String!
+  }
+
+  User {
+    _id:ID
+    username: String
+    email: String
+    bookCount: Integer
+    savedBooks: [Book]!
+  }
+
+  type Book {
+    bookId: ID!
+    authors: [String]
+    description: String
+    title: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+module.exports = typeDefs;
